1de54001112b3e58be26947ebeaffeac0a628610
Kale-Eos <pacoellaga@gmail.com>
2019-05-24 23:02:32 -0700

    Created basic time system started player controller (camera portions)

---
+++ ACGS_GameJam2019/Assets/Scripts/SunMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SunMovement : MonoBehaviour
+{
+    #region Variables
+    public Transform sunTransform;
+    public float speedVariable;
+    #endregion
+
+    #region Functions
+
+    void FixedUpdate()
+    {
+        sunTransform.Rotate(Vector3.down, speedVariable);
+        sunTransform.Rotate(Vector3.left, speedVariable);
+    }
+
+    #endregion
+}
